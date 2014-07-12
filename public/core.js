f0bd90a8aba8d83f4d47b36479cf134673a7ded2
Sahan <keeblersf@gmail.com>
2014-07-11 21:13:14 -0400

    Add ajax request to create new todo item

--- public/core.js
+++ public/core.js
@@ -14,6 +14,18 @@ function mainController($scope, $http) {
 						console.log('Error: ' + data);
 				});
 
-				
+		// when submitting the add form, send the text to the node API
+
+		$scope.createTodo = function(){
+				$http.post('/api/todos', $scope.formData)
+						.success(function(data){
+								$scope.formData = {}; // clear the form so user can enter another
+								$scope.todos = data;
+								console.log(data);
+						})
+						.error(function(data){
+								console.log('Error: ' + data);
+						});
+		};
 
 }
